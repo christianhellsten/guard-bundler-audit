062e2dfccb9e972d2a1f3600fec009b8e0ddde82
Christian Hellsten <christian@aktagon.com>
2014-04-25 06:50:07 +0000

    bundler-audit integration

--- lib/guard/bundler_audit.rb
+++ lib/guard/bundler_audit.rb
@@ -6,12 +6,35 @@ require 'bundler/audit/scanner'
 module Guard
   class BundlerAudit < Guard
     def start
+      Bundler::Audit::Database.update!
     end
 
     def run_all
+      audit
     end
 
     def run_on_changes paths
+      audit
+    end
+
+    def audit
+      res = Bundler::Audit::Scanner.new.scan.to_a.map do |vuln|
+        case vuln
+        when Bundler::Audit::Scanner::InsecureSource
+          "Insecure source URI found: #{vuln.source}"
+        when Bundler::Audit::Scanner::UnpatchedGem
+          "Insecure gem found: #{vuln.gem} #{vuln.advisory}"
+        end
+      end
+      if res.any?
+        message = res.join("\n")
+        notify message
+        fail message
+      end
+    end
+
+    def notify message
+      ::Guard::Notifier.notify(message, title: message, :image => :pending)
     end
   end
 end
